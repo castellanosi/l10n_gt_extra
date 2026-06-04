50b2e497d6019a434aef6a09b5418ef845032964
castellanosi <castellanosisaac62@gmail.com>
2026-06-04 23:07:25 +0000

    v5.42: eliminar reportes Inventario/FEL/Pequeño, preview Partida
    - Eliminados 12 archivos: reporte_inventario, reporte_fel, reporte_pequeno
    - Actualizados: __manifest__.py, report/__init__.py, wizard/__init__.py,
      security/ir.model.access.csv
    - Agregada acción qweb-html en reporte_partida_views.xml (vista previa)
    - Nueva migración 18.0.5.42: limpia registros UI en DBs existentes
    - Migración 19.0.0.0 ampliada para cubrir upgrade desde 17.x

--- migrations/19.0.0.0/pre-borrar-vistas.py
+++ migrations/19.0.0.0/pre-borrar-vistas.py
@@ -35,3 +35,26 @@ def migrate(cr, version):
     util.records.remove_record(cr, "l10n_gt_extra.action_reporte_ventas")
     util.records.remove_record(cr, "l10n_gt_extra.menu_asistente_reporte_ventas")
     _logger.info("Vistas viejas borradas")
+
+    # Reportes eliminados en 5.42 (FEL, Pequeño Contribuyente, Inventario IDs nuevos)
+    for xml_id in [
+        "l10n_gt_extra.reporte_fel_wizard_view_form",
+        "l10n_gt_extra.reporte_fel_wizard_action",
+        "l10n_gt_extra.reporte_fel_wizard_report",
+        "l10n_gt_extra.reporte_fel_wizard_menu",
+        "l10n_gt_extra.reporte_fel",
+        "l10n_gt_extra.reporte_pequeno_wizard_view_form",
+        "l10n_gt_extra.reporte_pequeno_wizard_action",
+        "l10n_gt_extra.reporte_pequeno_wizard_report",
+        "l10n_gt_extra.reporte_pequeno_wizard_menu",
+        "l10n_gt_extra.reporte_pequeno",
+        "l10n_gt_extra.reporte_inventario_wizard_view_form",
+        "l10n_gt_extra.reporte_inventario_wizard_action",
+        "l10n_gt_extra.reporte_inventario_wizard_report",
+        "l10n_gt_extra.reporte_inventario_wizard_menu",
+        "l10n_gt_extra.reporte_inventario",
+    ]:
+        try:
+            util.records.remove_record(cr, xml_id)
+        except Exception:
+            pass
